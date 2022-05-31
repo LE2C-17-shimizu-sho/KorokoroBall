e6d2520f242b4bd5ef7482cf6f9edde376cf3e08
ShoShimizu <k021g1379@g.neec.ac.jp>
2022-05-31 22:17:08 +0900

    評価課題

---
+++ Assets/Goal.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public GameObject clearText;
+    public GameObject nextButton;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ÉSÅ[ÉãÅI");
+
+        clearText.SetActive(true);
+        nextButton.SetActive(true);
+
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
